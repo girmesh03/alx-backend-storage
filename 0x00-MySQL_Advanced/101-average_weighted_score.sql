8831441aa1505065cd9822afd151e6d7cbc690f0
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-10 02:03:23 +0300

    101-average_weighted_score.sql updated #2

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,32 +4,38 @@
 	-- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
 
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-DELIMITER $$
+DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE user_id_var INT;
+    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE current_user_id INT;
+
+    -- Declare variables to store the total score and total weight for each user
     DECLARE total_score FLOAT;
     DECLARE total_weight INT;
     DECLARE avg_weighted_score FLOAT;
 
-    -- Cursor to iterate over users
-    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+    -- Declare continue handler for cursor
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-    -- Loop through each user
+    -- Open the cursor
     OPEN user_cursor;
+
+    -- Loop through users and calculate average weighted score
     user_loop: LOOP
-        FETCH user_cursor INTO user_id_var;
-        IF user_id_var IS NULL THEN
+        FETCH user_cursor INTO current_user_id;
+        IF done THEN
             LEAVE user_loop;
         END IF;
 
-        -- Calculate the total weighted score and total weight for the user
+        -- Calculate the total weighted score and total weight for the current user
         SELECT SUM(c.score * p.weight), SUM(p.weight)
         INTO total_score, total_weight
         FROM corrections c
         JOIN projects p ON c.project_id = p.id
-        WHERE c.user_id = user_id_var;
+        WHERE c.user_id = current_user_id;
 
         -- Calculate the average weighted score
         IF total_weight > 0 THEN
@@ -41,10 +47,12 @@ BEGIN
         -- Update the user's average_score in the users table
         UPDATE users
         SET average_score = avg_weighted_score
-        WHERE id = user_id_var;
-    END LOOP;
-    CLOSE user_cursor;
+        WHERE id = current_user_id;
+    END LOOP user_loop;
 
-END$$
+    -- Close the cursor
+    CLOSE user_cursor;
+END //
 
 DELIMITER ;
+
