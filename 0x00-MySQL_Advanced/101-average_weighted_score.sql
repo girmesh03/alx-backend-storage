36f86d4920743f4e6268b88d7e48ccb7fde30544
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-10 02:01:33 +0300

    101-average_weighted_score.sql updated #1

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,23 +3,48 @@
 -- Requirements:
 	-- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
 
-
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	DECLARE user_id INT;
-	DECLARE done INT DEFAULT FALSE;
-	DECLARE cur CURSOR FOR SELECT id FROM users;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-	OPEN cur;
-	read_loop: LOOP
-		FETCH cur INTO user_id;
-		IF done THEN
-			LEAVE read_loop;
-		END IF;
-		CALL ComputeAverageWeightedScoreForUser(user_id);
-	END LOOP;
-	CLOSE cur;
+    DECLARE user_id_var INT;
+    DECLARE total_score FLOAT;
+    DECLARE total_weight INT;
+    DECLARE avg_weighted_score FLOAT;
+
+    -- Cursor to iterate over users
+    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+
+    -- Loop through each user
+    OPEN user_cursor;
+    user_loop: LOOP
+        FETCH user_cursor INTO user_id_var;
+        IF user_id_var IS NULL THEN
+            LEAVE user_loop;
+        END IF;
+
+        -- Calculate the total weighted score and total weight for the user
+        SELECT SUM(c.score * p.weight), SUM(p.weight)
+        INTO total_score, total_weight
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id_var;
+
+        -- Calculate the average weighted score
+        IF total_weight > 0 THEN
+            SET avg_weighted_score = total_score / total_weight;
+        ELSE
+            SET avg_weighted_score = 0;
+        END IF;
+
+        -- Update the user's average_score in the users table
+        UPDATE users
+        SET average_score = avg_weighted_score
+        WHERE id = user_id_var;
+    END LOOP;
+    CLOSE user_cursor;
+
 END$$
 
 DELIMITER ;
