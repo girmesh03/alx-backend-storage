c68eb0e8b3a114e57b14e92982064971cad3bc56
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-10 02:16:07 +0300

    2-fans.sql updated #1

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -7,7 +7,7 @@
 -- Your script can be executed on any database
 
 
-SELECT origin, COUNT(*) AS nb_fans
-FROM bands
+SELECT origin, SUM(nb_fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
