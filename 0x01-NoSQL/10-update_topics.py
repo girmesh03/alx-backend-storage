01c88e16c82082f060e2ee75c87b0fb73bfe8c20
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-14 20:22:11 +0300

    10-update_topics.py added

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A Python function that changes all topics of a
+school document based on the name:"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """A Python function that changes all topics of a
+    school document based on the name:"""
+    if mongo_collection is None:
+        return []
+    return mongo_collection.update_many(
+        {"name": name}, {"$set": {"topics": topics}})
