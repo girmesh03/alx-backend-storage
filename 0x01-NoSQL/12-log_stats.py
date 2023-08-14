20194888e09db35bf1f81e963f8781f1f7277c4d
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-14 20:04:24 +0300

    12-log_stats.py added

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""A Python script that provides some stats
+about Nginx logs stored in MongoDB:"""
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    num_of_docs = collection.count_documents({})
+    print("{} logs".format(num_of_docs))
+
+    # methods list
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+
+    for method in methods_list:
+        method_count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+
+    # status check
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
