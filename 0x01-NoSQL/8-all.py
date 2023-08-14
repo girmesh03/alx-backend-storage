3f0050071165f2f7f867ffab19bb51f9cbe48107
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-14 20:17:18 +0300

    8-all.py added

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """A Python function that lists all documents in a collection"""
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
