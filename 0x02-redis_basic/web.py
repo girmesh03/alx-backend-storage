71ddc7a462533e998f07ae729dfee1c788c4bd8c
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-17 01:40:57 +0300

    web.py updated 3

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,58 +6,44 @@ Web Cache and Tracker
 import requests
 import redis
 import time
-from typing import Callable
+
+redis_client = redis.Redis()
+
 
 def get_page(url: str) -> str:
+    """Get a web page"""
+    # Create a key for tracking URL access count
+    count_key = f"count:{url}"
+
+    # Get the current count or initialize it if it doesn't exist
+    access_count = redis_client.get(count_key)
+    if access_count is None:
+        access_count = 1
+    else:
+        access_count = int(access_count) + 1
+
+    # Store the updated count in Redis
+    redis_client.set(count_key, access_count)
+
+    # Check if the page content is cached
+    cached_content = redis_client.get(url)
+    if cached_content is not None:
+        return cached_content.decode('utf-8')
+
+    # If not cached, fetch the page content using requests
     response = requests.get(url)
-    content = response.text
-    return content
+    page_content = response.text
 
-class WebCacheTracker:
-    def __init__(self):
-        self.redis_client = redis.Redis()
+    # Cache the page content with an expiration time of 10 seconds
+    redis_client.setex(url, 10, page_content)
 
-    def track_access_count(self, url: str):
-        access_key = f"count:{url}"
-        self.redis_client.incr(access_key)
+    return page_content
 
-    def cache_result(self, url: str, content: str):
-        cache_key = f"cache:{url}"
-        self.redis_client.setex(cache_key, 10, content)
-
-    def get_cached_page(self, url: str) -> str:
-        cache_key = f"cache:{url}"
-        cached_content = self.redis_client.get(cache_key)
-        if cached_content:
-            return cached_content.decode('utf-8')
-        return None
-
-def cached_web_request(fn: Callable):
-    cache_tracker = WebCacheTracker()
-
-    def wrapper(url: str):
-        cached_content = cache_tracker.get_cached_page(url)
-        if cached_content:
-            return cached_content
-
-        content = fn(url)
-        cache_tracker.cache_result(url, content)
-        return content
-
-    return wrapper
-
-@cached_web_request
-def get_page(url: str) -> str:
-    response = requests.get(url)
-    content = response.text
-    cache_tracker = WebCacheTracker()
-    cache_tracker.track_access_count(url)
-    return content
 
 if __name__ == "__main__":
-    slow_url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.google.com"
+    url = "http://slowwly.robertomurray.co.uk"
 
-    for _ in range(5):
-        page_content = get_page(slow_url)
-        print(page_content)
+    for _ in range(3):
+        content = get_page(url)
+        print(content)
         time.sleep(2)
