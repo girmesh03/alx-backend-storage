d38d1af6d080e506f845b17200f562144118824d
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-17 00:50:54 +0300

    web.py updated #1

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python3
-""" Web cache example using Redis"""
-import redis
+"""
+Web Cache and Tracker
+"""
+
 import requests
+import redis
 import time
 
 class WebCache:
-    """ Web cache class"""
+    """
+    Class to fetch, cache, and track web pages.
+    """
+
     def __init__(self):
-        """ Init method"""
+        """
+        Initializes the WebCache instance.
+        """
         self._redis = redis.Redis()
 
-    def get_page(self, url: str) -> str:
-        """ Get page method"""
-        # Check if the cached content exists
+    def cached_page(self, url: str) -> str:
+        """
+        Fetches the content of a web page, caches it, and tracks its access count.
+
+        Args:
+            url (str): The URL of the web page to fetch.
+
+        Returns:
+            str: The content of the web page.
+        """
         cached_content = self._redis.get(url)
         if cached_content:
             return cached_content.decode('utf-8')
 
-        # Fetch the page content
         response = requests.get(url)
         content = response.text
 
-        # Store the content with an expiration time of 10 seconds
         self._redis.setex(url, 10, content)
-
-        # Track the access count
-        access_count_key = f"count:{url}"
-        self._redis.incr(access_count_key)
+        self._redis.incr(f"count:{url}")
 
         return content
 
+    def get_page(self, url: str) -> str:
+        """
+        Fetches the content of a web page, using caching and tracking.
+
+        Args:
+            url (str): The URL of the web page to fetch.
+
+        Returns:
+            str: The content of the web page.
+        """
+        return self.cached_page(url)
+
 if __name__ == "__main__":
     web_cache = WebCache()
-
-    urls = ["http://slowwly.robertomurray.co.uk/\
-            delay/5000/url/https://www.example.com"]  # Example slow URL
-    for url in urls:
-        content = web_cache.get_page(url)
-        print(content)
-
-        access_count_key = f"count:{url}"
-        access_count = web_cache._redis.get(access_count_key).decode('utf-8')
-        print(f"Access count for {url}: {access_count}")
-
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.example.com"
+    content = web_cache.get_page(url)
+    print(content)
