2492b7a392c8a8377b20529593cbcff0906f87b6
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-17 01:35:42 +0300

    web.py updated 2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,53 +7,43 @@ import requests
 import redis
 import time
 
-class WebCache:
-    """
-    Class to fetch, cache, and track web pages.
-    """
+redis_client = redis.Redis()
 
-    def __init__(self):
-        """
-        Initializes the WebCache instance.
-        """
-        self._redis = redis.Redis()
 
-    def cached_page(self, url: str) -> str:
-        """
-        Fetches the content of a web page, caches it, and tracks its access count.
+def get_page(url: str) -> str:
+    """Get a web page"""
+    # Create a key for tracking URL access count
+    count_key = f"count:{url}"
 
-        Args:
-            url (str): The URL of the web page to fetch.
+    # Get the current count or initialize it if it doesn't exist
+    access_count = redis_client.get(count_key)
+    if access_count is None:
+        access_count = 1
+    else:
+        access_count = int(access_count) + 1
 
-        Returns:
-            str: The content of the web page.
-        """
-        cached_content = self._redis.get(url)
-        if cached_content:
-            return cached_content.decode('utf-8')
+    # Store the updated count in Redis
+    redis_client.set(count_key, access_count)
 
-        response = requests.get(url)
-        content = response.text
+    # Check if the page content is cached
+    cached_content = redis_client.get(url)
+    if cached_content is not None:
+        return cached_content.decode('utf-8')
 
-        self._redis.setex(url, 10, content)
-        self._redis.incr(f"count:{url}")
+    # If not cached, fetch the page content using requests
+    response = requests.get(url)
+    page_content = response.text
 
-        return content
+    # Cache the page content with an expiration time of 10 seconds
+    redis_client.setex(url, 10, page_content)
 
-    def get_page(self, url: str) -> str:
-        """
-        Fetches the content of a web page, using caching and tracking.
+    return page_content
 
-        Args:
-            url (str): The URL of the web page to fetch.
-
-        Returns:
-            str: The content of the web page.
-        """
-        return self.cached_page(url)
 
 if __name__ == "__main__":
-    web_cache = WebCache()
-    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.example.com"
-    content = web_cache.get_page(url)
-    print(content)
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.google.com"
+
+    for _ in range(3):
+        content = get_page(url)
+        print(content)
+        time.sleep(2)
